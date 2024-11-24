64e6ea81354af36cca7aa21f267efb07aff2509f
ipekm <ipek.motorcu@ozu.edu.tr>
2024-11-24 18:56:21 +0300

    Updated Users

--- app/src/main/java/com/cs394/socialmediaapp/Controller/PostController.kt
+++ app/src/main/java/com/cs394/socialmediaapp/Controller/PostController.kt
@@ -8,28 +8,28 @@ class PostController {
     fun getSamplePosts(): List<Post> {
         return listOf(
             Post(
-                username = "user1",
+                username = "Emin",
                 time = System.currentTimeMillis(),
                 caption = "This is a sample post",
                 likes = 10,
                 userid = "user1_id",
-                downloadUrl = R.drawable.sahil
+                downloadUrl = R.drawable.book
             ),
             Post(
-                username = "user2",
+                username = "Sabina",
                 time = System.currentTimeMillis(),
                 caption = "Another example post",
                 likes = 25,
                 userid = "user2_id",
-                downloadUrl = R.drawable.sahil
+                downloadUrl = R.drawable.paint
             ),
             Post(
-                username = "user3",
+                username = "İpek",
                 time = System.currentTimeMillis(),
                 caption = "Loving this new app!",
                 likes = 50,
                 userid = "user3_id",
-                downloadUrl = R.drawable.sahil
+                downloadUrl = R.drawable.sea
             )
         )
     }
